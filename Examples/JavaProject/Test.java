d4af5b1a1b3528c46ba0c538f69ba5d91e124422
Jakob Hansen <jakob.hansen@hotmail.no>
2022-04-02 14:35:44 +0200

    Add presentation

---
+++ Examples/JavaProject/Test.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+
+public class Test {
+    private String hello;
+
+    public static void main(String[] args) {
+        String hello = "hello";
+        hello.length();
+        List<String> goodbye;
+        
+    }
+
+    public Test(String hello) {
+        this.hello = hello;
+    }
+}
